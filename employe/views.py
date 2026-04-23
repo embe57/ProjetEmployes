9ee995136870f624b344401a8b0b06d010902a4c
embe57 <embe57@gmail.com>
2026-04-23 22:22:11 +0200

    ajout login required

--- employe/views.py
+++ employe/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from .models import Employe
 from .forms import EmployeForm
-# fonction qui récupère la liste des employés
+
+@login_required
 def liste_employes(request):
     employes = Employe.objects.all()
     return render(request, 'employe/list.html', {'employes' : employes})
 
+@login_required
 def ajouter_employe(request):
     form = EmployeForm(request.POST or None)
     if form.is_valid():
@@ -13,6 +16,7 @@ def ajouter_employe(request):
         return redirect('liste_employes')
     return render(request, 'employe/formulaire.html', {'form' : form})
 
+@login_required
 def modifier_employe(request, id):
     employe = get_object_or_404(Employe, id=id)
     form = EmployeForm(request.POST or None, instance=employe)
@@ -21,6 +25,7 @@ def modifier_employe(request, id):
         return redirect('liste_employes')
     return render(request, 'employe/formulaire.html', {'form': form})
 
+@login_required
 def supprimer_employe(request, id):
     employe = get_object_or_404(Employe, id=id)
     if request.method == "POST":
